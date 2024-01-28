9717eae0e6961988e8e2782e5f3cffdde6f14fcd
ReyeMe <gphys25@gmail.com>
2024-01-28 12:11:46 +0100

    Removed duplicate that got to the worng place by mistake when cleaning up

--- saturnringlib/srl_debug.hpp
+++ saturnringlib/srl_debug.hpp
@@ -271,41 +271,4 @@ namespace SRL
 		#define Assert(message) ()
 #endif
 	};
-
-	/** @brief Structure data loader by dannyduarte
-	 */
-	namespace StructLoader
-	{
-		/** @brief Check whether struct contains size function
-		 * @tparam T Struct type
-		 */
-		template<typename T>
-		concept HasLoadSizeFunction = requires {
-			{ std::declval<T>().LoadSize() } -> std::same_as<size_t>;
-		};
-
-		/** @brief Get struct pointer and iterate buffer pointer
-		 * @tparam T Struct type
-		 * @param iterator Data buffer
-		 * @param count Struct size
-		 * @return T* Struct pointer
-		 */
-		template<typename T>
-		T* GetAndIterate(char*& iterator, size_t count = 1)
-		{
-			T* ptr = reinterpret_cast<T*>(iterator);
-
-			if constexpr (HasLoadSizeFunction<T>)
-			{
-				iterator += ptr->LoadSize() * count;
-			}
-			else
-			{
-				iterator += (sizeof(T) * count);
-			}
-
-			return ptr;
-		}
-
-	}
 }
