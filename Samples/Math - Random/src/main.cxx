5f28e16434262135c20574f5a66d4a24d05741a0
ReyeMe <gphys25@gmail.com>
2024-05-09 11:28:54 +0200

    Added random number generator with sample

---
+++ Samples/Math - Random/src/main.cxx
@@ -0,0 +1,66 @@
+#include <srl.hpp>
+
+// Using to shorten names for Vector and HighColor
+using namespace SRL::Types;
+
+// Main program entry
+int main()
+{
+	SRL::Core::Initialize(HighColor(20,10,50));
+    SRL::Debug::Print(1,1, "Random number generator sample");
+
+    // Initialize random number generator with a specific seed
+    SRL::Math::Random rnd = SRL::Math::Random(15);
+
+    // Define variables to keep track of random data
+    Sint32 data[320];
+    Uint32 pointer = 0;
+
+    // Update delay counter
+    Uint32 tm = 0;
+    
+    // Main program loo
+	while(1)
+	{
+        // Generate new random number after a specified delay
+        if (tm++ > 2)
+        {
+            // Generate new random number
+            data[pointer] = rnd.GetNumber(-25,25);
+            
+            // Print new random number
+            SRL::Debug::Print(1,3, "Random: %d    ", data[pointer]);
+
+            // Move data pointer
+            pointer++;
+
+            if (pointer >= 320)
+            {
+                pointer = 0;
+            }
+
+            // Reset delay counter
+            tm = 0;
+        }
+
+        // Draw graph
+        for (int i = 0; i < 319; i++)
+        {
+            SRL::Render::DrawLine(
+                Vector2D(Fxp::FromInt(i- 160), Fxp::FromInt(data[i])),
+                Vector2D(Fxp::FromInt(i + 1 - 160), Fxp::FromInt(data[i + 1])),
+                HighColor::Colors::White,
+                500.0);
+        }
+        
+        SRL::Render::DrawLine(
+            Vector2D(Fxp::FromInt(pointer - 160), Fxp::FromInt(50)),
+            Vector2D(Fxp::FromInt(pointer - 160), Fxp::FromInt(-50)),
+            HighColor::Colors::Red,
+            500.0);
+
+        SRL::Core::Synchronize();
+	}
+
+	return 0;
+}
--- saturnringlib/srl_math.hpp
+++ saturnringlib/srl_math.hpp
@@ -2,6 +2,7 @@
 
 #include "srl_base.hpp"
 #include "srl_fxp.hpp"
+#include "srl_datetime.hpp"
 
 namespace SRL
 {
@@ -54,5 +55,112 @@ namespace SRL
 		{
 			return Math::Min(maximum, Math::Max(minimum, value));
 		}
+
+        /** @brief Pseudo-Random number generator
+         * @note This pseudo number generator is done by using LFSR (see https://en.wikipedia.org/wiki/Linear-feedback_shift_register)
+         */
+        class Random
+        {
+        private:
+
+            /** @brief Fallback number choosen by different random number generator I stumbled on online
+             */
+            inline static const Uint32 FallbackRandomNumber = 0x7c1ec949u;
+
+            /** @brief Pseudo-Random number generator seed
+             */
+            Uint32 startState;
+
+            /** @brief Pseudo-Random number generator seed
+             */
+            Uint32 seed;
+
+            /** @brief Get next pseudo-random number
+             * @return Generated number
+             */
+            Uint32 GetNextPeriod()
+            {
+                Uint32 lfsr = this->startState;
+
+                // Start state must not be 0
+                if (lfsr == 0)
+                {
+                    // Seed is 0, just fall back
+                    if (this->seed == 0)
+                    {
+                        lfsr = Random::FallbackRandomNumber;
+                    }
+                    else
+                    {
+                        lfsr = this->seed;
+                    }
+                }
+
+                Uint32 bit;
+                Uint32 period = 0;
+
+                do
+                {
+                    bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ^ (lfsr >> 16) ^ (lfsr >> 17) ^ (lfsr >> 18) ^ (lfsr >> 19)) & 1u;
+                    lfsr = (lfsr >> 1) | (bit << 31);
+                    ++period;
+                }
+                while (lfsr == this->startState);
+
+                this->startState = lfsr;
+                return lfsr;
+            }
+
+        public:
+        
+            /** @brief Construct a new pseudo-random number generator
+             */
+            Random()
+            {
+                Types::DateTime time = Types::DateTime::Now();
+                this->startState = (time.Year() << 9) +
+                    (time.Month() << 5) +
+                    time.Day() +
+                    time.Hour() +
+                    time.Minute() +
+                    time.Second();
+                
+                this->seed = this->startState;
+                this->startState = this->GetNextPeriod();
+            }
+
+            /** @brief Construct a new pseudo-random number generator
+             * @param seed Starting seed
+             */
+            Random(Uint32 seed)
+            {
+                this->startState = seed;
+                this->seed = seed;
+                this->startState = this->GetNextPeriod();
+            }
+
+            /** @brief Destroy the pseudo-random number generator object
+             */
+            ~Random() { }
+
+            /** @brief Get next pseudo-random number
+             * @return Generated number in a full range
+             */
+            Uint32 GetNumber()
+            {
+                return this->GetNextPeriod();
+            }
+            
+            /** @brief Get next pseudo-random number
+             * @param from Inclusive start of the range
+             * @param to Inclusive end of the range
+             * @return Generated number in a range
+             */
+            Sint32 GetNumber(Sint32 from, Sint32 to)
+            {
+                Uint32 number = this->GetNumber();
+                return from + (number % Math::Abs(to - from));
+            }
+        };
 	};
 }
