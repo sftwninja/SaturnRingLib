307910319039585887d74e482b9fa6c0f7002e1a
ReyeMe <gphys25@gmail.com>
2024-07-07 16:12:47 +0200

    DrawSprite project renamed to VDP1 - Sprite effects

--- Samples/VDP1 - Sprite effects/src/main.cxx
+++ Samples/VDP1 - Sprite effects/src/main.cxx
@@ -1,43 +1,56 @@
 #include <srl.hpp>
 
+// Using to shorten names for Vector and HighColor
 using namespace SRL::Types;
 
+// Main program entry
 int main()
 {
-	SRL::Core::Initialize(HighColor(20,10,50));
-    SRL::Bitmap::TGA* tga = new SRL::Bitmap::TGA("TEST.TGA");   // Loads TGA file into main RAM
-    int32_t textureIndex = SRL::VDP1::TryLoadTexture(tga);       // Loads TGA into VDP1
-    delete tga;                                                 // Frees main RAM
+	SRL::Core::Initialize(HighColor::Colors::Black);
+    SRL::Debug::Print(1,1, "VDP1 sprite effects sample");
 
+    // Load sprite image
+    SRL::Bitmap::TGA* tga = new SRL::Bitmap::TGA("TEST.TGA");
+    int32_t textureIndex = SRL::VDP1::TryLoadTexture(tga);
+    delete tga;
+
+    // Initialize gouraud table entries
     HighColor * table = SRL::VDP1::GetGouraudTable();
+
+    // 1st entry
     table[0] = HighColor::Colors::White;
     table[1] = HighColor::Colors::Red;
     table[2] = HighColor::Colors::Green;
     table[3] = HighColor::Colors::Yellow;
 
+    // 3rd entry
     table[8] = HighColor::Colors::Magenta;
     table[9] = HighColor::Colors::Magenta;
     table[10] = HighColor::Colors::Green;
     table[11] = HighColor::Colors::Green;
 
+    // Start with no rotation
     uint16_t angle = 0;
 
+    // Start main program loop
 	while(1)
 	{
 
+        // Draw first sprite, use first entry in gouraud table
         SRL::Scene2D::SetEffect(SRL::Scene2D::SpriteEffect::Gouraud, 0);
-        SRL::Scene2D::DrawSprite(textureIndex, Vector3D(0.0, 0.0, 500.0));
+        SRL::Scene2D::DrawSprite(textureIndex, Vector3D(0.0, -60.0, 500.0));
 
-
+        // Draw second sprite, rotate it, use third gouraud table entry, enable screen doors (mesh) effect, and also scale it
         SRL::Scene2D::SetEffect(SRL::Scene2D::SpriteEffect::Gouraud, 2);
         SRL::Scene2D::SetEffect(SRL::Scene2D::SpriteEffect::ScreenDoors, true);
-        SRL::Scene2D::DrawSprite(textureIndex, Vector3D(0.0, -60.0, 500.0), Angle::FromRaw(angle), Vector2D(0.75, 1.5));
+        SRL::Scene2D::DrawSprite(textureIndex, Vector3D(0.0, 0.0, 500.0), Angle::FromRaw(angle), Vector2D(0.75, 1.5));
         SRL::Scene2D::SetEffect(SRL::Scene2D::SpriteEffect::ScreenDoors, false);
 
-
+        // Draw third sprite with no effect applied
         SRL::Scene2D::SetEffect(SRL::Scene2D::SpriteEffect::Gouraud, -1);
         SRL::Scene2D::DrawSprite(textureIndex, Vector3D(0.0, 60.0, 500.0));
 
+        // Increase rotation angle
         angle+=100;
 
         SRL::Core::Synchronize();
