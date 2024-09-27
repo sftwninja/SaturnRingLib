3591be751a689bbf76bab5d6b3339532602e5c9a
willll <464311+willll@users.noreply.github.com>
2024-09-27 15:02:28 -0400

    Code review

--- Samples/SH2 - Slave/src/main.cxx
+++ Samples/SH2 - Slave/src/main.cxx
@@ -4,55 +4,55 @@
 using namespace SRL::Types;
 
 /** @brief Task Obeject
- */
+*/
 class Task : public SRL::ITask
 {
 public:
 
-	/** @brief Constructor
-	 */
-	Task() : cpt(0) {}
+    /** @brief Constructor
+    */
+    Task() : cpt(0) {}
 
-	void Do()
-	{
-		SRL::Debug::Print(1,4, "%03d, %s : Line %d : %s()", cpt++, __FILE__, __LINE__ , __func__ );
-	}
+    void Do()
+    {
+        SRL::Debug::Print(1,4, "%03d, %s : Line %d : %s()", cpt++, __FILE__, __LINE__ , __func__ );
+    }
 
-	uint8_t GetCounter()
-	{
-		return cpt;
-	}
+    uint8_t GetCounter()
+    {
+        return cpt;
+    }
 
 protected:
-	uint8_t cpt;;
+    uint8_t cpt;;
 };
 
 // Main program entry
 int main()
 {
-	Task task;
+    Task task;
 
-	SRL::Core::Initialize(HighColor(20,10,50));
-	SRL::Debug::Print(1,1, "SH2 Slave");
+    SRL::Core::Initialize(HighColor(20,10,50));
+    SRL::Debug::Print(1,1, "SH2 Slave");
 
-	// Main program loo
-	while(1)
-	{
-		// Clear the screen
-		SRL::Debug::PrintClearScreen();
+    // Main program loo
+    while(1)
+    {
+        // Clear the screen
+        SRL::Debug::PrintClearScreen();
 
-		SRL::Debug::Print(1,3, "SH2 Slave sample");
+        SRL::Debug::Print(1,3, "SH2 Slave sample");
 
-		SRL::Slave::ExecuteOnSlave(task);
+        SRL::Slave::ExecuteOnSlave(task);
 
-		// Wait for the task to be executed
-		while(!task.IsDone());
+        // Wait for the task to be executed
+        while(!task.IsDone());
 
-		// Display the counter increased by the task
-		SRL::Debug::Print(1,5, "Counter increased by Slave : %d", task.GetCounter());
+        // Display the counter increased by the task
+        SRL::Debug::Print(1,5, "Counter increased by Slave : %d", task.GetCounter());
 
-		SRL::Core::Synchronize();
-	}
+        SRL::Core::Synchronize();
+    }
 
-	return 0;
+    return 0;
 }
--- saturnringlib/srl_slave.hpp
+++ saturnringlib/srl_slave.hpp
@@ -1,82 +1,82 @@
 #pragma once
 
 extern "C" {
-  #include <sgl.h>  // For slSlaveFunc
+    #include <sgl.h>  // For slSlaveFunc
 }
 
 /** @brief Main SGL namespace
 */
 namespace SRL
 {
-  /** @brief Abstract class that defines the class prototype to implement
-  *          a Task that runs on Slave SH2
-  */
-  class ITask
-  {
-  public:
-    /** @brief Constructor
+    /** @brief Abstract class that defines the class prototype to implement
+    *          a Task that runs on Slave SH2
     */
-    ITask() : _done(false) {}
-
-    /** @brief Destructor
-    */
-    virtual ~ITask() {}
-
-    /** @brief Task Status getter
-    * @returns Task Status
-    */
-    virtual bool IsDone()
+    class ITask
     {
-      return _done;
-    }
+    public:
+        /** @brief Constructor
+        */
+        ITask() : _done(false) {}
 
-    /** @brief Start the Task on Slave SH2, then set its status to Done
+        /** @brief Destructor
+        */
+        virtual ~ITask() {}
+
+        /** @brief Task Status getter
+        * @returns Task Status
+        */
+        virtual bool IsDone()
+        {
+            return _done;
+        }
+
+        /** @brief Start the Task on Slave SH2, then set its status to Done
+        */
+        virtual void Start()
+        {
+            this->Do();
+            _done = true;
+        }
+
+        /** @brief Reset Task Status before running
+        */
+        virtual void ResetTask()
+        {
+            _done = false;
+        }
+
+    protected:
+        volatile bool  _done;
+
+        /** @brief Absctract method that defines the task's behavior
+        */
+        virtual void Do() = 0;
+    };
+
+    /** @brief Core functions of the library
     */
-    virtual void Start()
+    class Slave
     {
-      this->Do();
-      _done = true;
-    }
+    private:
 
-    /** @brief Reset Task Status before running
-    */
-    virtual void ResetTask()
-    {
-      _done = false;
-    }
+        /** @brief Internal Wrapper function executed on Slave SH2 CPU
+        */
+        inline static void SlaveTask(void * pTask)
+        {
+            ITask * task = static_cast<ITask *>(pTask);
+            task->Start();
+        }
 
-  protected:
-    volatile bool  _done;
+    public:
 
-    /** @brief Absctract method that defines the task's behavior
-    */
-    virtual void Do() = 0;
-  };
+        /** @brief API call to execute an ITask onto Slave SH2
+        * @param task ITask object to be executed
+        */
+        inline static void ExecuteOnSlave(ITask & task)
+        {
+            task.ResetTask();
+            slSlaveFunc(SlaveTask, static_cast<void *>(&task));
+        }
 
-  /** @brief Core functions of the library
-  */
-  class Slave
-  {
-  private:
-
-    /** @brief Internal Wrapper function executed on Slave SH2 CPU 
-    */
-    inline static void SlaveTask(void * pTask)
-    {
-      ITask * task = static_cast<ITask *>(pTask);
-      task->Start();
-    }
-
-  public:
-
-    /** @brief API call to execute an ITask onto Slave SH2
-     * @param task ITask object to be executed
-    */
-    inline static void ExecuteOnSlave(ITask & task)
-    {
-      task.ResetTask();
-      slSlaveFunc(SlaveTask, static_cast<void *>(&task));
-    }
-
-  };
+    };
 };
