42eeff8cbf0c5e861e38a51dfee9cb7affba4e65
7shades <burlingameross@gmail.com>
2024-10-10 15:07:19 -0700

    Minonr Bugfixes to Samples

--- Samples/VDP2 - Bmp2Tile/src/main.cxx
+++ Samples/VDP2 - Bmp2Tile/src/main.cxx
@@ -8,13 +8,15 @@ using namespace SRL::Types;
 using namespace SRL::Input;
 
 //example loading the weapon sprites for right hand
-void LoadRightHand(char* img)
+void LoadRightHand(const char* img)
 {
     //load TGA file:
     SRL::Bitmap::TGA* MyBmp = new SRL::Bitmap::TGA(img);
-    //convert to tilemap with 5 pages(Source Pattern+1 page per animation frame):
+    
+    //create tilemap from bmp with 5 pages(Source image Pattern+ 1 page per animation frame):
     SRL::Tilemap::Bmp2Tile* Tile = new SRL::Tilemap::Bmp2Tile(*MyBmp,5);
-    delete MyBmp;//no longer need original bitmap
+    delete MyBmp;//no longer need original bitmap in memory
+    
     //copy map of each individual frame from source page to the top left corner its own dedicated page:
     //(the tiles created are 16*16 pixels , so the 128*128 pixel frames have area of 8x8 tile coordinates)
     Tile->CopyMap(0, SRL::Tilemap::Co(0, 0), SRL::Tilemap::Co(7, 7), 1, SRL::Tilemap::Co(0, 0));
@@ -22,14 +24,16 @@ void LoadRightHand(char* img)
     Tile->CopyMap(0, SRL::Tilemap::Co(16,0), SRL::Tilemap::Co(23, 7), 3, SRL::Tilemap::Co(0, 0));
     Tile->CopyMap(0, SRL::Tilemap::Co(24,0), SRL::Tilemap::Co(31, 7), 4, SRL::Tilemap::Co(0, 0));
     
-    //We will display right hand on NBG1 Layer:
+    //We will display right hand on NBG1 Layer in this example:
     SRL::VDP2::NBG1::LoadTilemap(*Tile);
+    
     //allign Layer to display in bottom right corner of screen:
     Vector2D pos(-200.0, -100.0);
     SRL::VDP2::NBG1::SetPosition(pos);
-    //Set to display only the page of the first frame:
+    
+    //Set to display only the page containing the first frame:
     SRL::VDP2::NBG1::SetMapLayout(1, 1, 1, 1);
-    delete Tile;//Tilemap has been loaded to VRAM, no longer it in work ram (unless you want to edit it further)
+    delete Tile;//Tilemap has been loaded to VRAM, no longer need in work ram (unless you want to edit it further)
 }
 //example animating right weapon 
 void SetFrameRight(uint8_t frame)
@@ -44,24 +48,16 @@ int main()
     SRL::VDP2::NBG0::CelAddress = (void*)VDP2_VRAM_A0;
     SRL::VDP2::NBG0::MapAddress = (void*)VDP2_VRAM_A1;
     
-    //Load Bitmap from CD to Work RAM 
-    //SRL::Bitmap::TGA* test = new SRL::Bitmap::TGA("FOG.TGA");
-    //Copy Bitmap to a Tilemap in Main Work Ram
-    //SRL::Tilemap::Bmp2Tile * TestBmp2Tile = new SRL::Tilemap::Bmp2Tile(*test,1);
-    //delete test;//No need to keep original bitmap after conversion
-    
-    //manipulate tilemap by copying tiled patterns between its pages:
-    //TestBmp2Tile->CopyMap(0, SRL::Tilemap::Co(0, 0), SRL::Tilemap::Co(7, 7), 0, SRL::Tilemap::Co(8, 8));
-    
-    //Load the Tilemap from Work RAM to VRAM registered with NBG0
-    //SRL::VDP2::NBG0::LoadTilemap(*TestBmp2Tile);
+ 
     LoadRightHand("WPN.TGA");
-    SRL::VDP2::NBG1::SetPriority(SRL::VDP2::Priority::Layer2);//set NBG0 priority
-    SRL::VDP2::NBG1::ScrollEnable();//enable display of NBG0 
+   
+    SRL::VDP2::NBG1::SetPriority(SRL::VDP2::Priority::Layer2);//set NBG1 priority
+    SRL::VDP2::NBG1::ScrollEnable();//enable display of NBG1
     
-   // delete TestBmp2Tile;
-    Vector2D pos = Vector2D(0.0, 0.0);
+  
     uint8_t frame = 1;
+    SRL::Debug::Print(1, 1, "Tilemap Page Animation");
+    SRL::Debug::Print(1, 3, "Press R to Cycle 4 animation frames");
     //Main Game Loop 
     while(1)
     {
@@ -71,7 +67,6 @@ int main()
             if (frame > 4) frame = 1;
             SetFrameRight(frame);
         }
-        //pos += Vector2D(1.0, 1.0);
         
         SRL::Core::Synchronize();
 
