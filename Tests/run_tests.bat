b26fc824e41c3383e1a8d164bb17507a07031a48
willll <464311+willll@users.noreply.github.com>
2024-11-09 16:17:19 -0500

    wip

--- Tests/run_tests.bat
+++ Tests/run_tests.bat
@@ -1,7 +1,21 @@
 
 @goto(){
   # Linux code here
+  command="prog -foo -whatever"
+  log="prog.log"
+  match="this is the what i want to match"
 
+  $command > "$log" 2>&1 &
+  pid=$!
+
+  while sleep 60
+  do
+      if fgrep --quiet "$match" "$log"
+      then
+          kill $pid
+          exit 0
+      fi
+  done
 
 }
 
