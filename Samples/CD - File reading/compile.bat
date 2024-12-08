851de76c476495f2f943246afc51ea00715303ce
willll <464311+willll@users.noreply.github.com>
2024-12-08 11:22:45 -0500

    Fixed compilation script for linux

--- Samples/CD - File reading/compile.bat
+++ Samples/CD - File reading/compile.bat
@@ -1,21 +1,46 @@
-@ECHO Off
+@goto(){
+  # Linux code here
 
-SET COMPILER_DIR=../../Compiler
-SET PATH=%COMPILER_DIR%\Other Utilities;%PATH%
-SET PATH=%COMPILER_DIR%\msys2\usr\bin;%PATH%
-SET PATH=%COMPILER_DIR%\sh2eb-elf\bin;%PATH%
+  export COMPILER_DIR=../../Compiler
+  export PATH=${COMPILER_DIR}/linux/sh2eb-elf/bin:${PATH}
 
-IF "%1" == "debug" GOTO debug
-IF "%1" == "release" GOTO release
+  if [[ $# -eq 0 ]]; then
+    echo "Not enough arguments"
+    exit 1
+  fi
 
-:debug
-powershell write-host -back Yellow -fore Black Building debug...
-make all DEBUG=1
-GOTO end
+  if [[ "$1" = "debug" ]]; then
+    echo "BUILD IN DEBUG : $1"
+    make all DEBUG=1 || exit
+  elif [[ "$1" = "release" ]]; then
+    echo "BUILD IN RELEASE"
+    make all || exit
+  fi
+}
 
-:release
-powershell write-host -back Green -fore White Building release...
-make all
-GOTO end
+@goto $@
+exit
 
-:end
+:(){
+
+    @ECHO Off
+
+    SET COMPILER_DIR=../../Compiler
+    SET PATH=%COMPILER_DIR%\Other Utilities;%PATH%
+    SET PATH=%COMPILER_DIR%\msys2\usr\bin;%PATH%
+    SET PATH=%COMPILER_DIR%\sh2eb-elf\bin;%PATH%
+
+    IF "%1" == "debug" GOTO debug
+    IF "%1" == "release" GOTO release
+
+    :debug
+    powershell write-host -back Yellow -fore Black Building debug...
+    make all DEBUG=1
+    GOTO end
+
+    :release
+    powershell write-host -back Green -fore White Building release...
+    make all
+    GOTO end
+
+    :end
