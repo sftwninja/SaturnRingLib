f6e01b3bd6cc34cfb42d728216c3c56e99fe8691
willll <464311+willll@users.noreply.github.com>
2024-12-14 10:04:39 -0500

    Fixed typo for linux build script

--- tools/scripts/make.sh
+++ tools/scripts/make.sh
@@ -14,17 +14,17 @@ if [[ $# -eq 0 ]]; then
   make all DEBUG=1 || exit
 fi
 
-if [[ "$1" -eq "debug" ]]; then
+if [[ "$1" = "debug" ]]; then
   printf "\033[30m\033[43mBuilding debug...\033[0m\r\n"
   make all DEBUG=1 || exit
 fi
 
-if [[ "$1" -eq "release" ]]; then
+if [[ "$1" = "release" ]]; then
   printf "\033[30m\033[42mBuilding release...\033[0m\r\n"
   make all || exit
 fi
 
-if [[ "$1" -eq "clean" ]]; then
+if [[ "$1" = "clean" ]]; then
   printf "\033[30m\033[105mCleaning...\033[0m\r\n"
   make clean || exit
 fi
