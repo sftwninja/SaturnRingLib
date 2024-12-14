3e2f268df9ba3eb32f37a40004ab65e006b8c4cb
willll <464311+willll@users.noreply.github.com>
2024-12-14 10:56:47 -0500

    use new scripts

--- Tests/clean.bat
+++ Tests/clean.bat
@@ -1,25 +1,3 @@
-
-@goto(){
-  # Linux code here
-  export COMPILER_DIR=../Compiler
-  export PATH=${COMPILER_DIR}/linux/sh2eb-elf/bin:${PATH}
-
-  make clean || exit
-}
-
-@goto $@
-exit
-
-:(){
-  @echo off
-  rem Windows script here
-  SET COMPILER_DIR=../Compiler
-  SET PATH=%COMPILER_DIR%\Other Utilities;%PATH%
-  SET PATH=%COMPILER_DIR%\msys2\usr\bin;%PATH%
-  SET PATH=%COMPILER_DIR%\sh2eb-elf\bin;%PATH%
-
-  powershell write-host -back Yellow -fore Black Building debug...
-  make clean DEBUG=1
-  GOTO end
-
-  :end
+:; "../tools/scripts/make.sh" clean ../Compiler; exit;
+@ECHO Off
+"../tools/scripts/make.bat" clean ../Compiler
