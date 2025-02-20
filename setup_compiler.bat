43344b21568a916af913472798911c96c46fcf76
ReyeMe <24783344+ReyeMe@users.noreply.github.com>
2025-02-20 21:59:35 +0100

    Fix the download script by Danny

--- setup_compiler.bat
+++ setup_compiler.bat
@@ -1,3 +1,3 @@
 :; "./tools/scripts/getcompiler.sh" "14.2.0"; exit;
 @ECHO Off
-powershell "./tools/scripts/getcompiler.ps1" "14.2.0"
+PowerShell -ExecutionPolicy Bypass -file "./tools/scripts/getcompiler.ps1" "14.2.0"
